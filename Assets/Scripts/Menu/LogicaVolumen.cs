ed9fd96fc54e6e3e722dbfaeafde9264a5790163
Dragomir Raicevich <dragoraicevich@hotmail.com>
2025-06-05 23:51:42 -0300

    Menu_Parte_01

---
+++ Assets/Scripts/Menu/LogicaVolumen.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogicaVolumen : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Slider slider;
+    public float sliderValue;
+    public Image imagenMute;
+    void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("volumenAudio", 0.5f);
+        AudioListener.volume = slider.value;
+        RevisarSiEstoyMute();
+        
+    }
+
+    public void ChangeSlider(float valor)
+    {
+        sliderValue = valor;
+        PlayerPrefs.SetFloat("volumenAudio", sliderValue);
+        AudioListener.volume = slider.value;
+        RevisarSiEstoyMute();
+    }
+
+    public void RevisarSiEstoyMute()
+    {
+        if (sliderValue == 0)
+        {
+            imagenMute.enabled = true;
+        }
+        else
+        {
+            imagenMute.enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
